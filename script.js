901658e4713aabf71dc1037d1cc55e5062a5f890
Glitch (glitch-hello-website) <none>
2023-02-08 10:31:58 +0000

    😇🏙 Checkpoint ./index.html:1106040/8554 ./script.js:1106040/3020 ./style.css:1106040/2589

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const root = document.documentElement;
- 
-document.addEventListener('mousemove', evt => {
-    let x = evt.clientX / innerWidth;
-    let y = evt.clientY / innerHeight;
- 
-    root.style.setProperty('--mouse-x', x);
-    root.style.setProperty('--mouse-y', y);
-});
-
-
 
 const followers = document.querySelectorAll(".follower");
 
 document.addEventListener("mousemove", (evt) => {
   let x = evt.clientX;
   let y = evt.clientY;
+
+  let x2 = evt.clientX / innerWidth;
+  let y2 = evt.clientY / innerHeight;
+
+  root.style.setProperty("--mouse-x", x2);
+  root.style.setProperty("--mouse-y", y2);
   
-  followers.forEach(follower => {
+  followers.forEach((follower) => {
     follower.style.left = x + "px";
     follower.style.top = y + "px";
   });
