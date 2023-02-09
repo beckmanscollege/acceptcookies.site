739771f4a361a7ca525e3954f462ed8ec792b26f
Glitch (glitch-hello-website) <none>
2023-02-09 10:53:10 +0000

    ⏰⏲ Checkpoint ./script.js:49247821/1793 ./index.html:49247821/17 ./style.css:49247821/119

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
 const root = document.documentElement;
 
-const followers = document.querySelectorAll(".follower1");
-
-document.addEventListener("mousemove", (evt) => {
-  let x = evt.clientX;
-  let y = evt.clientY;
-
-  let x2 = evt.clientX / innerWidth;
-  let y2 = evt.clientY / innerHeight;
-
-  root.style.setProperty("--mouse-x", x2);
-  root.style.setProperty("--mouse-y", y2);
-  
-  followers.forEach((follower) => {
-    follower.style.left = x + "px";
-    follower.style.top = y + "px";
-  });
-  
-  const followers = document.querySelectorAll(".follower1");
-
-document.addEventListener("mousemove", (evt) => {
-  let x = evt.clientX;
-  let y = evt.clientY;
-
-  let x2 = evt.clientX / innerWidth;
-  let y2 = evt.clientY / innerHeight;
-
-  root.style.setProperty("--mouse-x", x2);
-  root.style.setProperty("--mouse-y", y2);
-  
-  followers.forEach((follower) => {
-    follower.style.left = x + "px";
-    follower.style.top = y + "px";
-  });
-  
-  const followers = document.querySelectorAll(".follower1");
+const followers = document.querySelectorAll(".follower");
 
 document.addEventListener("mousemove", (evt) => {
   let x = evt.clientX;
